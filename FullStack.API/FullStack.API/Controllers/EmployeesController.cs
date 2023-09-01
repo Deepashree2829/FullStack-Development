e0de2dafc5f18c78a4a0db300e049f3e622cbb3f
Deepashree <bkdeeepashree2829@gmail.com>
2023-09-01 21:06:52 +0530

    Created new .NET proj in VS 2022

---
+++ FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using FullStack.API.Data;
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.id = Guid.NewGuid();
+            await _fullStackDbContext.Employees.AddAsync(employeeRequest);
+            await _fullStackDbContext.SaveChangesAsync();
+
+            return Ok(employeeRequest);
+        }
+    }
+}
---
+++ FullStack.API/FullStack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FullStack.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
